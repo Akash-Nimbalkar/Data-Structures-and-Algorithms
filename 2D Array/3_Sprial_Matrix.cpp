d2d53c35a3d4f5ea608cd7379fb8ac877ba06095
Akash-Nimbalkar <akashnimbalkar259@gmail.com>
2024-03-04 12:49:08 +0530

    Added 3_Sprial_Matrix.cpp

---
+++ 2D Array/3_Sprial_Matrix.cpp
@@ -0,0 +1,78 @@
+// Given an m x n matrix, return all elements of the matrix in spiral order.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> spiralOrder(vector<vector<int>> &matrix)
+{
+
+    vector<int> ans;
+
+    int row = matrix.size();
+    int col = matrix[0].size();
+
+    int count = 0;
+    int total = row * col;
+
+    int sRow = 0;
+    int eRow = row - 1;
+    int sCol = 0;
+    int eCol = col - 1;
+
+    while (count < total)
+    {
+        for (int i = sCol; count < total && i <= eCol; i++)
+        {
+            ans.push_back(matrix[sRow][i]);
+            count++;
+        }
+        sRow++;
+
+        for (int i = sRow; count < total && i <= eRow; i++)
+        {
+            ans.push_back(matrix[i][eCol]);
+            count++;
+        }
+        eCol--;
+
+        for (int i = eCol; count < total && i >= sCol; i--)
+        {
+            ans.push_back(matrix[eRow][i]);
+            count++;
+        }
+        eRow--;
+
+        for (int i = eRow; count < total && i >= sRow; i--)
+        {
+            ans.push_back(matrix[i][sCol]);
+            count++;
+        }
+        sCol++;
+    }
+    return ans;
+}
+int main()
+{
+    int m, n;
+    cout << "Enter no. of rows and columns " << endl;
+    cin >> m >> n;
+
+    vector<vector<int>> matrix(m, vector<int>(n));
+
+    cout << "Enter the elements of the matrix:" << endl;
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> matrix[i][j];
+        }
+    }
+    vector<int> ans = spiralOrder(matrix);
+
+    for (int i = 0; i < ans.size(); i++)
+    {
+        cout << ans[i] << " ";
+    }
+
+    return 0;
+}
